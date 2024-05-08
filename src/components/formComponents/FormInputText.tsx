87ff195a0b62c0fc55af60dd009683f962ac49a0
Sergey <seleznev-krista@mail.ru>
2024-05-08 23:19:31 +0300

    feat: add component FormInputText

---
+++ src/components/formComponents/FormInputText.tsx
@@ -0,0 +1,44 @@
+import { Control, Controller, FieldErrors, RegisterOptions } from 'react-hook-form';
+
+import TextField from '@mui/material/TextField';
+
+import { RegistrationForm } from '@/types/interfaces';
+
+type FormInputProps = {
+  control: Control<RegistrationForm>;
+  errors: FieldErrors<RegistrationForm>;
+  label: string;
+  name: keyof RegistrationForm;
+  rules: RegisterOptions;
+  type: string;
+};
+
+export const FormInputText = ({
+  control,
+  errors,
+  label,
+  name,
+  rules,
+  type,
+}: FormInputProps): JSX.Element => {
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({ field: { onChange, value } }) => (
+        <TextField
+          error={!!errors.surname?.message}
+          fullWidth
+          helperText={errors?.surname?.message}
+          label={label}
+          onChange={onChange}
+          size="small"
+          type={type}
+          value={value}
+          variant="outlined"
+        />
+      )}
+      rules={rules}
+    />
+  );
+};
