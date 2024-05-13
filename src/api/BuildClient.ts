a57874ab9425082d2e79ec90c77e06b84c54276a
gryzun33 <65145953+gryzun33@users.noreply.github.com>
2024-05-13 11:16:08 +0300

    feat: RSS-ECOMM-2_64 add working with sdk (#35)
    * feat: add sdk files
    
    * refactor: rename function

---
+++ src/api/BuildClient.ts
@@ -0,0 +1,70 @@
+import {
+  AnonymousAuthMiddlewareOptions,
+  AuthMiddlewareOptions,
+  HttpMiddlewareOptions,
+  PasswordAuthMiddlewareOptions,
+} from '@commercetools/sdk-client-v2';
+import fetch from 'node-fetch';
+
+const PROJECT_KEY = import.meta.env.VITE_PROJECT_KEY;
+const SCOPES = [import.meta.env.VITE_SCOPES];
+const API_URL = import.meta.env.VITE_API_URL;
+const AUTH_URL = import.meta.env.VITE_AUTH_URL;
+const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
+const CLIENT_SECRET = import.meta.env.VITE_CLIENT_SECRET;
+
+// Configure authMiddlewareOptions
+export const authMiddlewareOptions: AuthMiddlewareOptions = {
+  host: AUTH_URL,
+  projectKey: PROJECT_KEY,
+  credentials: {
+    clientId: CLIENT_ID,
+    clientSecret: CLIENT_SECRET,
+  },
+  scopes: SCOPES,
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  fetch,
+};
+
+// Configure httpMiddlewareOptions
+export const httpMiddlewareOptions: HttpMiddlewareOptions = {
+  host: API_URL,
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  fetch,
+};
+
+// authanonym
+export const authAnonymMiddlewareOptions: AnonymousAuthMiddlewareOptions = {
+  host: AUTH_URL,
+  projectKey: PROJECT_KEY,
+  credentials: {
+    clientId: CLIENT_ID,
+    clientSecret: CLIENT_SECRET,
+    anonymousId: crypto.randomUUID(),
+  },
+  scopes: SCOPES,
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  fetch,
+};
+
+// authuser
+export const authUserMiddlewareOptions = (
+  email: string,
+  password: string,
+): PasswordAuthMiddlewareOptions => {
+  return {
+    host: AUTH_URL,
+    projectKey: PROJECT_KEY,
+    credentials: {
+      clientId: CLIENT_ID,
+      clientSecret: CLIENT_SECRET,
+      user: {
+        username: email,
+        password,
+      },
+    },
+    scopes: SCOPES,
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    fetch,
+  };
+};
---
+++ src/api/clientService.ts
@@ -0,0 +1,84 @@
+import {
+  ByProjectKeyRequestBuilder,
+  createApiBuilderFromCtpClient,
+} from '@commercetools/platform-sdk';
+import { Client, ClientBuilder } from '@commercetools/sdk-client-v2';
+
+import { LoginForm } from '@/types/interfaces';
+
+import {
+  authAnonymMiddlewareOptions,
+  authMiddlewareOptions,
+  authUserMiddlewareOptions,
+  httpMiddlewareOptions,
+} from './BuildClient';
+
+const ctpClient = new ClientBuilder()
+  .withClientCredentialsFlow(authMiddlewareOptions)
+  .withHttpMiddleware(httpMiddlewareOptions)
+  .withLoggerMiddleware()
+  .build();
+
+let apiRoot = getApiRoot(ctpClient);
+
+function getApiRoot(ctpClient: Client): ByProjectKeyRequestBuilder {
+  return createApiBuilderFromCtpClient(ctpClient).withProjectKey({
+    projectKey: import.meta.env.VITE_PROJECT_KEY,
+  });
+}
+
+export function anonymFlowAuth(): ByProjectKeyRequestBuilder {
+  const ctpClient = new ClientBuilder()
+    .withClientCredentialsFlow(authAnonymMiddlewareOptions)
+    .withHttpMiddleware(httpMiddlewareOptions)
+    .withLoggerMiddleware()
+    .build();
+
+  apiRoot = getApiRoot(ctpClient);
+
+  return apiRoot;
+}
+
+export function passwordFlowAuth({ email, password }: LoginForm): ByProjectKeyRequestBuilder {
+  const ctpClient = new ClientBuilder()
+    .withClientCredentialsFlow(authUserMiddlewareOptions(email, password))
+    .withHttpMiddleware(httpMiddlewareOptions)
+    .withLoggerMiddleware()
+    .build();
+
+  apiRoot = getApiRoot(ctpClient);
+
+  return apiRoot;
+}
+
+export async function getProject(): Promise<void> {
+  return apiRoot.get().execute().then(console.log).catch(console.error);
+}
+
+export async function loginUser({ email, password }: LoginForm): Promise<void> {
+  return apiRoot
+    .me()
+    .login()
+    .post({
+      body: {
+        email,
+        password,
+      },
+    })
+    .execute()
+    .then(console.log)
+    .catch(console.error);
+}
+
+export async function checkUserEmail(email: string): Promise<void> {
+  return apiRoot
+    .customers()
+    .get({
+      queryArgs: {
+        where: [`email="${email}"`],
+      },
+    })
+    .execute()
+    .then(console.log)
+    .catch(console.error);
+}
