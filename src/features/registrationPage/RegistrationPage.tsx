467b2b1848ec33b1a140b02fb7c9c9e3acccd66c
Sergey <seleznev-krista@mail.ru>
2024-05-17 18:22:05 +0300

    feat: changes message output

--- src/features/registrationPage/RegistrationPage.tsx
+++ src/features/registrationPage/RegistrationPage.tsx
@@ -8,25 +8,41 @@ import ModalMessage from '@/components/modalMessage/modalMessage';
 import FormOfRegistration from './registrationForm/RegistrationForm';
 
 export default function RegistrationPage(): JSX.Element {
+  const DELAY_CLOSE_MESSAGES = 2000;
+  const [hasRegFormError, setHasRegFormError] = useState(false);
+  console.log('render hasRegFormError:', hasRegFormError);
+
   const navigation = useNavigate();
   const [open, setOpen] = useState(false);
   const handleOpen = (): void => setOpen(true);
   const handleClose = (): void => {
     setOpen(false);
-    navigation('/');
+    console.log('hasRegFormError', hasRegFormError);
+    if (!hasRegFormError) {
+      navigation('/');
+    }
   };
   const [message, setMessage] = useState('');
 
-  const resultOfSubmit = (result: { error: boolean; message: string }): void => {
-    console.log('result', result.error, result.message);
+  const resultOfSubmit = (result: { hasError: boolean; message: string }): void => {
+    //   setHasRegFormError(result.hasError)
+    setHasRegFormError(result.hasError);
     setMessage(result.message);
     handleOpen();
+    if (!result.hasError) {
+      setTimeout(handleClose, DELAY_CLOSE_MESSAGES);
+    }
   };
 
   return (
     <Container sx={{ height: '100vh' }}>
       <FormOfRegistration resultOfSubmit={resultOfSubmit} />
-      <ModalMessage handleClose={handleClose} message={message} open={open} />
+      <ModalMessage
+        handleClose={handleClose}
+        hasError={hasRegFormError}
+        message={message}
+        open={open}
+      />
     </Container>
   );
 }
