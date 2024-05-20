36b694cc02d35af7f0c67b46013ded657b85ff4b
alekseng <alexvdche@mail.ru>
2024-05-20 22:00:32 +0300

    feat: add profile, cart and about page. feat: on the home page, add links to all pages.

--- src/features/mainPage/MainPage.tsx
+++ src/features/mainPage/MainPage.tsx
@@ -1,12 +1,37 @@
-import { Box, Container, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+import { Box, Container, List, Typography } from '@mui/material';
 
 import flower from '../../assets/images/flowers.jpg';
-import { accent, description, left, main, right, subtitle, title } from './Styles';
+import { li, ul } from '../layout/Header/Styles';
+import { accent, description, left, linkList, main, right, subtitle, title, topEl } from './Styles';
 import { textContent } from './TextContent';
 
 function MainPage(): JSX.Element {
   return (
     <Container component="section" sx={main}>
+      <Box component="nav" sx={topEl}>
+        <List component="ul" sx={[ul, linkList]}>
+          <Typography component="li" sx={li}>
+            <Link to="/">Main</Link>
+          </Typography>
+          <Typography component="li" sx={li}>
+            <Link to="/login">Login</Link>
+          </Typography>
+          <Typography component="li" sx={li}>
+            <Link to="/registration">Registration</Link>
+          </Typography>
+          <Typography component="li" sx={li}>
+            <Link to="/profile">Profile</Link>
+          </Typography>
+          <Typography component="li" sx={li}>
+            <Link to="/cart">Cart</Link>
+          </Typography>
+          <Typography component="li" sx={li}>
+            <Link to="/about">About</Link>
+          </Typography>
+        </List>
+      </Box>
       <Box component="div" sx={left}>
         <Typography component="span" sx={subtitle}>
           {textContent.subtitle}
