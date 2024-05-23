998a2588eed416b47f660db772d54d96df2d91bb
Sergey <seleznev-krista@mail.ru>
2024-05-23 05:14:56 +0300

    feat: add NoAuthPanel.test, RegistrationPage.test

---
+++ src/features/registrationPage/RegistrationPage.test.tsx
@@ -0,0 +1,19 @@
+import { MemoryRouter } from 'react-router-dom';
+
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+
+import RegistrationPage from './RegistrationPage';
+
+describe('ButtonToAnotherPage', () => {
+  it('should render', () => {
+    render(
+      <MemoryRouter>
+        <RegistrationPage />
+      </MemoryRouter>,
+    );
+
+    const buttonText = screen.getByText('Registration');
+    expect(buttonText).toBeInTheDocument();
+  });
+});
