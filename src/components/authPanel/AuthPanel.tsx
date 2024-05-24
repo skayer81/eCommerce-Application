a82e69d72d1d9a01181704d1767355688a1b8729
gryzun33 <yakushevao777@gmail.com>
2024-05-24 12:22:05 +0300

    refactor: delete comments

--- src/components/authPanel/AuthPanel.tsx
+++ src/components/authPanel/AuthPanel.tsx
@@ -2,22 +2,13 @@ import { Link } from 'react-router-dom';
 
 import { Button } from '@mui/material';
 
-// import { anonymFlowAuth } from '@/api/clientService';
 import { button } from '@/features/layout/Header/Styles';
-// import { useUserStore } from '@/stores/userStore';
 
 interface AuthPanelProps {
   logout: () => void;
 }
 
 export default function AuthPanel({ logout }: AuthPanelProps): JSX.Element {
-  // const { logoutUserInStore } = useUserStore();
-
-  // const logout = (): void => {
-  //   logoutUserInStore();
-  //   anonymFlowAuth();
-  // };
-
   return (
     <>
       <Button color="success" onClick={logout} sx={button} variant="contained">
