ef65bddb56091775b66db84ada1ba35984b5981a
gryzun33 <yakushevao777@gmail.com>
2024-05-26 22:43:25 +0300

    feat: add erroralert

---
+++ src/components/errorAlert/ErrorAlert.tsx
@@ -0,0 +1,14 @@
+// import Button from '@mui/material/Button';
+// import Snackbar from '@mui/material/Snackbar';
+import { Container } from '@mui/material';
+import Alert from '@mui/material/Alert';
+
+export default function ErrorAlert(): JSX.Element {
+  return (
+    <Container sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+      <Alert severity="error" sx={{ width: '300px', mt: '100px' }}>
+        Something went wrong. Try again later...
+      </Alert>
+    </Container>
+  );
+}
--- src/features/catalogPage/CatalogPage.tsx
+++ src/features/catalogPage/CatalogPage.tsx
@@ -1,10 +1,72 @@
-import { Container, Typography } from '@mui/material';
+import {
+  ClientResponse,
+  ProductProjection,
+  ProductProjectionPagedQueryResponse,
+} from '@commercetools/platform-sdk';
+import { Grid, Paper, Typography } from '@mui/material';
+import { useQuery } from '@tanstack/react-query';
+// import { useState } from 'react';
+
+import { getProducts } from '@/api/clientService';
+import ErrorAlert from '@/components/errorAlert/ErrorAlert';
+
+import ProductCard from './ProductCard';
 
 function CatalogPage(): JSX.Element {
+  const { data, isSuccess, isError, error } = useQuery<
+    ClientResponse<ProductProjectionPagedQueryResponse>
+  >({
+    queryKey: ['products'],
+    queryFn: getProducts,
+  });
+
+  if (isSuccess) {
+    console.log('products=', data.body.results);
+  }
+
+  if (isError) {
+    console.error(error);
+    return <ErrorAlert />;
+  }
+
   return (
-    <Container component="section">
-      <Typography component="p">page under development</Typography>
-    </Container>
+    <>
+      <Grid container spacing={2}>
+        {/* Первая колонка */}
+        <Grid item xs={2}>
+          <Paper>
+            <Typography variant="h6">Первая колонка</Typography>
+            <Typography>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Typography>
+          </Paper>
+        </Grid>
+        {/* Вторая колонка */}
+        <Grid item xs={10}>
+          <Grid container direction="column" spacing={2}>
+            {/* Первый ряд второй колонки */}
+            <Grid item>
+              <Paper>
+                <Typography variant="h6">Вторая колонка - Ряд 1</Typography>
+                <Typography>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Typography>
+              </Paper>
+            </Grid>
+            {/* Второй ряд второй колонки */}
+            <Grid item>
+              <Grid container spacing={2}>
+                {data?.body.results.map((item: ProductProjection) => (
+                  <ProductCard
+                    description={item.metaDescription?.en}
+                    imageUrl={item.masterVariant.images?.[0].url}
+                    key={item.key}
+                    name={item.name.en}
+                    price={item.masterVariant.prices?.[0].value.centAmount}
+                  />
+                ))}
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
