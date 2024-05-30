3212533fd5a9633c9aa82161c33046448c9d0c7a
Sergey <seleznev-krista@mail.ru>
2024-05-30 12:41:25 +0300

    fix: del comments

--- src/features/productPage/ProductPage.tsx
+++ src/features/productPage/ProductPage.tsx
@@ -1,46 +1,14 @@
-import { Container } from '@mui/material';
-//import { useEffect, useState } from 'react';
-
-import { getProductByKey } from '../../api/clientService';
-//import { AddBoxOutlined, BorderAll, ImageAspectRatioOutlined, Title } from "@mui/icons-material";
-import ProductCard from './ProductCard';
-//import { useQuery } from "@tanstack/react-query";
-
-// import {
-//   ClientResponse,
-//   ProductProjection,
-//   ProductProjectionPagedQueryResponse,
-// } from '@commercetools/platform-sdk';
-// import { Paper, } from '@mui/material';
 import { ClientResponse, Product } from '@commercetools/platform-sdk';
+import { Container } from '@mui/material';
 import { useQuery } from '@tanstack/react-query';
 
-// import { useState } from 'react';
-
-//import { getProducts } from '@/api/clientService';
-//import  ErrorAlert from '@/components/errorAlert/ErrorAlert';
-
-//import ProductCard from './ProductCard';
+import { getProductByKey } from '../../api/clientService';
+import ProductCard from './ProductCard';
 
 type Props = {
   productKey: string;
 };
 
-// type getProductByKeyBody = {
-//   masterData:{
-//     current:{
-//       name:{
-//         en:string
-//       }
-//     }
-//   }
-// }
-
-// type CurrentProperties = {
-//   name:{
-//     en:string
-//   }
-// }
 type AtribListItem = {
   name: string;
   value: {
@@ -66,29 +34,11 @@ function productAdapter(data: ClientResponse): ProductProperties {
 }
 
 export default function ProductPage({ productKey = 'ficus-elastica5' }: Props): JSX.Element {
-  // const [productProperty, setProductProperty] =  useState();
-  // const product = getProductByKey('ficus-elastica5').then((value) =>{
-  // //  console.log('свойства' ,value.body)//?.masterData?.current)
-  //   let test:CurrentProperties = value.body?.masterData?.current;
-  //   console.log('test', test);
-  //  // setProductProperty(test);
-  //  // setProductProperty(value);
-  // }).catch(() => {
-
-  // }) ;
-  // console.log(product);
-  //https://api.europe-west1.gcp.commercetools.com/fun-code-shop/products/key=ficus-elastica5
-
   const item = 'ficus-elastica5';
-  const { data, isSuccess, error, isPending } = useQuery({
+  const { data, error, isPending } = useQuery({
     queryKey: ['products', { key: productKey }],
     queryFn: () => getProductByKey(item),
-  }); // https://api.europe-west1.gcp.commercetools.com/fun-code-shop/products/key=%5Bobject%20Object%5D
-  //let product: Product;
-  if (isSuccess) {
-    // //    product  = data?.body
-    console.log('isSuccess products=', data);
-  }
+  });
 
   if (isPending) {
     return <Container>Loading...</Container>;
@@ -98,16 +48,9 @@ export default function ProductPage({ productKey = 'ficus-elastica5' }: Props): 
     return <Container> `An error has occurred: ${error.message}`</Container>;
   }
 
-  //if (data !== undefined){
-
   return (
     <Container sx={{ border: 1 }}>
       <ProductCard productProps={productAdapter(data)} />
-
-      {/* <ProductCard list={е}></ProductCard> */}
     </Container>
   );
-  // }
 }
-
-// list={data?.body.masterData.current?.masterVariant?.attributes} name={data?.body.masterData.current.name.en}
