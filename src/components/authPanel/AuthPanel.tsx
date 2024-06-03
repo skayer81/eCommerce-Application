3909c34949286f23d46c8d01960d4105d83afad3
gryzun33 <65145953+gryzun33@users.noreply.github.com>
2024-06-03 14:35:56 +0300

    feat: add profile icon (#83)

--- src/components/authPanel/AuthPanel.tsx
+++ src/components/authPanel/AuthPanel.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 
-import { Button } from '@mui/material';
+import { AccountCircle } from '@mui/icons-material';
+import { Button, IconButton } from '@mui/material';
 
 import { button } from '@/features/layout/Header/Styles';
 
@@ -11,8 +12,11 @@ interface AuthPanelProps {
 export default function AuthPanel({ logout }: AuthPanelProps): JSX.Element {
   return (
     <>
-      <Button color="success" onClick={logout} sx={button} variant="contained">
-        <Link to="/">Logout</Link>
+      <IconButton component={Link} to="/profile">
+        <AccountCircle color="primary" fontSize="large" />
+      </IconButton>
+      <Button color="primary" onClick={logout} sx={button} variant="contained">
+        <Link to="/">Log out</Link>
       </Button>
     </>
   );
