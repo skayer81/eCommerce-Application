564b6a5b658a45dc4803fc5d35900274db03f8dc
Sergey <seleznev-krista@mail.ru>
2024-06-09 11:50:02 +0300

    feat: add basketPageListItem

---
+++ src/features/basketPage/basketPageListItem.tsx
@@ -0,0 +1,35 @@
+import { Box, Card, CardContent, CardMedia, ListItem, Typography } from '@mui/material';
+
+import { BasketDataList } from './basketTypes';
+
+export function BasketPageListItem({ listItem }: { listItem: BasketDataList }): JSX.Element {
+  return (
+    <ListItem sx={{ display: 'flex', justifyContent: 'center' }}>
+      <Card>
+        <Box sx={{ width: '100%' }}>
+          <CardMedia
+            component="img"
+            height="200"
+            image={listItem.img ?? 'https://placehold.co/1000x1000?text=No+Image'}
+            sx={{ objectFit: 'contain' }}
+            title={listItem.name}
+          />
+        </Box>
+      </Card>
+      <CardContent>
+        <Typography component="div" gutterBottom sx={{ lineHeight: '1.3' }} variant="h6">
+          name: {listItem.name}
+        </Typography>
+        <Typography component="div" sx={{ lineHeight: '1.3' }} variant="h6">
+          price: {(listItem.price / 1000).toFixed(2) + '$'}
+        </Typography>
+        <Typography component="div" sx={{ lineHeight: '1.3' }} variant="h6">
+          quantity:{listItem.quantity}
+        </Typography>
+        <Typography component="div" sx={{ lineHeight: '1.3' }} variant="h6">
+          total price:{((listItem.quantity * listItem.price) / 1000).toFixed(2) + '$'}
+        </Typography>
+      </CardContent>
+    </ListItem>
+  );
+}
