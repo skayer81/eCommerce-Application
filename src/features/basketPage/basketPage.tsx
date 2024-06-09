066358c2742bafb1a89728b4630bc58e453416ed
Sergey <seleznev-krista@mail.ru>
2024-06-09 10:44:17 +0300

    feat: add basketPage adapter

--- src/features/basketPage/basketPage.tsx
+++ src/features/basketPage/basketPage.tsx
@@ -1,5 +1,39 @@
-import { Container } from '@mui/material';
+import { Container, Typography } from '@mui/material';
+import { useQuery } from '@tanstack/react-query';
+
+import { useUserStore } from '@/stores/userStore';
+
+import { getCustomerBasket } from '../../api/clientService';
+import basketDataAdapter from './basketDataAdapter';
 
 export function BasketPage(): JSX.Element {
-  return <Container>BasketPage</Container>;
+  const userID = useUserStore().userId;
+
+  const { data, error, isPending } = useQuery({
+    queryKey: ['customer', userID],
+    queryFn: () => getCustomerBasket(userID),
+    select: basketDataAdapter,
+  });
+
+  if (isPending) {
+    return <Container>Loading...</Container>;
+  }
+
+  if (error) {
+    return <Container> `An error has occurred: ${error.message}`</Container>;
+  }
+
+  console.log(data);
+
+  return (
+    <Container sx={{ border: 1, padding: 2 }}>
+      {data.map((item, index) => {
+        return (
+          <Container key={index}>
+            <Typography>111</Typography>
+          </Container>
+        );
+      })}
+    </Container>
+  );
 }
