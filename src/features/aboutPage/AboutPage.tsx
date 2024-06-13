92fc66cf2407b356d469894d4a723ef4a814a70e
alekseng <alexvdche@mail.ru>
2024-06-13 22:14:56 +0300

    feat: add school logo

--- src/features/aboutPage/AboutPage.tsx
+++ src/features/aboutPage/AboutPage.tsx
@@ -1,9 +1,11 @@
 import DeveloperCard from './DeveloperCard';
+import Rsschool from './Rsscool';
 
 function AboutPage(): JSX.Element {
   return (
     <>
       <DeveloperCard />
+      <Rsschool />
     </>
   );
 }
