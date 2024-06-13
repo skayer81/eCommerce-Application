6a2ca3c82edb1e557ac0bd77feae90efd47bf54e
Sergey <seleznev-krista@mail.ru>
2024-06-13 22:14:55 +0300

    fix: types of basket items

--- src/features/basketPage/basketTypes.ts
+++ src/features/basketPage/basketTypes.ts
@@ -1,7 +1,15 @@
-export type BasketDataList = {
+export type BasketDataItem = {
+  ID: string;
+  discount: number | undefined;
   img: string;
   name: string;
   price: number;
   quantity: number;
   sku: string;
+  // totalPrice: number;
+};
+
+export type BasketData = {
+  basketItems: Array<BasketDataItem>;
+  totalBasketPrice: number;
 };
