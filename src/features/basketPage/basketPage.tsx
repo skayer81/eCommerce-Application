b9be8349ffcb2d066bd16b50492cef86cf9e464e
Sergey <seleznev-krista@mail.ru>
2024-06-13 19:07:48 +0300

    fix: ButtonChangeQuantity

--- src/features/basketPage/basketPage.tsx
+++ src/features/basketPage/basketPage.tsx
@@ -3,18 +3,18 @@ import { useQuery } from '@tanstack/react-query';
 
 import ErrorAlert from '@/components/errorAlert/ErrorAlert';
 import Loader from '@/components/loader/Loader';
-import { useUserStore } from '@/stores/userStore';
+import { useBasketStore } from '@/stores/basketStore';
 
-import { getCustomerBasket } from '../../api/clientService';
+import { getUserBasket } from '../../api/clientService';
 import basketDataAdapter from './basketDataAdapter';
 import { BasketPageList } from './basketPageList';
 
 export function BasketPage(): JSX.Element {
-  const userID = useUserStore().userId;
+  const { basketId } = useBasketStore();
 
   const { data, error, isPending } = useQuery({
-    queryKey: ['customer', userID],
-    queryFn: () => getCustomerBasket(userID),
+    queryKey: ['basketList', basketId],
+    queryFn: () => getUserBasket(basketId),
     select: basketDataAdapter,
   });
 
@@ -34,7 +34,13 @@ export function BasketPage(): JSX.Element {
       <Typography align="center" component="h1" variant="h4">
         Basket
       </Typography>
-      <BasketPageList listData={data} />
+      {data.length === 0 ? (
+        <Typography align="center" component="p" variant="h5">
+          this message indicating that the cart is empty
+        </Typography>
+      ) : (
+        <BasketPageList listData={data} />
+      )}
     </Container>
   );
 }
--- src/features/basketPage/basketPageListItem.tsx
+++ src/features/basketPage/basketPageListItem.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-
 import { Box, Card, CardContent, CardMedia, ListItem, Typography } from '@mui/material';
 
 import ButtonChangeQuantity from '@/components/buttonsForBasket/ButtonChangeQuantity';
@@ -8,51 +6,34 @@ import { BasketDecIncButtons } from './basketDecIncButtons';
 import { BasketDataList } from './basketTypes';
 
 export function BasketPageListItem({ listItem }: { listItem: BasketDataList }): JSX.Element {
-  const [isVisible, setIsVisible] = useState(true);
   return (
-    <>
-      {!isVisible ? (
-        ''
-      ) : (
-        <ListItem sx={{ display: 'flex', justifyContent: 'center' }}>
-          <Card>
-            <Box sx={{ width: '100%' }}>
-              <CardMedia
-                component="img"
-                height="200"
-                image={listItem.img ?? 'https://placehold.co/1000x1000?text=No+Image'}
-                sx={{ objectFit: 'contain' }}
-                title={listItem.name}
-              />
-            </Box>
-          </Card>
-          <CardContent>
-            <Typography component="div" gutterBottom sx={{ lineHeight: '1.3' }} variant="h6">
-              name: {listItem.name}
-            </Typography>
-            <Typography component="div" sx={{ lineHeight: '1.3' }} variant="h6">
-              price: {(listItem.price / 1000).toFixed(2) + '$'}
-            </Typography>
-            <BasketDecIncButtons
-              quantity={listItem.quantity}
-              sku={listItem.sku}
-            ></BasketDecIncButtons>
-            <Typography component="div" sx={{ lineHeight: '1.3' }} variant="h6">
-              total price:{((listItem.quantity * listItem.price) / 1000).toFixed(2) + '$'}
-            </Typography>
-            <ButtonChangeQuantity
-              callback={() => {
-                setIsVisible(false);
-              }}
-              disabled={false}
-              quantity={0}
-              sku={listItem.sku}
-            >
-              {'Remove from Cart'}
-            </ButtonChangeQuantity>
-          </CardContent>
-        </ListItem>
-      )}
-    </>
+    <ListItem sx={{ display: 'flex', justifyContent: 'center' }}>
+      <Card>
+        <Box sx={{ width: '100%' }}>
+          <CardMedia
+            component="img"
+            height="200"
+            image={listItem.img ?? 'https://placehold.co/1000x1000?text=No+Image'}
+            sx={{ objectFit: 'contain' }}
+            title={listItem.name}
+          />
+        </Box>
+      </Card>
+      <CardContent>
+        <Typography component="div" gutterBottom sx={{ lineHeight: '1.3' }} variant="h6">
+          name: {listItem.name}
+        </Typography>
+        <Typography component="div" sx={{ lineHeight: '1.3' }} variant="h6">
+          price: {(listItem.price / 1000).toFixed(2) + '$'}
+        </Typography>
+        <BasketDecIncButtons quantity={listItem.quantity} sku={listItem.sku}></BasketDecIncButtons>
+        <Typography component="div" sx={{ lineHeight: '1.3' }} variant="h6">
+          total price:{((listItem.quantity * listItem.price) / 1000).toFixed(2) + '$'}
+        </Typography>
+        <ButtonChangeQuantity disabled={false} quantity={0} sku={listItem.sku}>
+          {'Remove from Cart'}
+        </ButtonChangeQuantity>
+      </CardContent>
+    </ListItem>
   );
 }
