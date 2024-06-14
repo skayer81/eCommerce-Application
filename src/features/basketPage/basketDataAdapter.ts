331cd81f34f71c90f82b6c0fae4a09e63873b5f5
skayer81 <114329023+skayer81@users.noreply.github.com>
2024-06-14 10:28:58 +0300

    fix: fix type, fix adapter (#109)

--- src/features/basketPage/basketDataAdapter.ts
+++ src/features/basketPage/basketDataAdapter.ts
@@ -3,6 +3,7 @@ import { Cart, ClientResponse, Image, LineItem } from '@commercetools/platform-s
 import { BasketData, BasketDataItem } from './basketTypes';
 
 function basketDataAdapter(data: ClientResponse<Cart>): BasketData {
+  console.log('пришло в адаптер', data);
   const cart: Array<LineItem> = data.body.lineItems;
   const basketItems: Array<BasketDataItem> = [];
   cart.forEach((lineItem) => {
@@ -10,20 +11,32 @@ function basketDataAdapter(data: ClientResponse<Cart>): BasketData {
     const image: Image | undefined = images ? images[0] : undefined;
     const basketItem: BasketDataItem = {
       ID: lineItem.id,
-      discount: lineItem.totalPrice.centAmount, // prices ? prices[0].discounted?.value.centAmount : undefined;
+      discount: lineItem.totalPrice.centAmount,
       img: image?.url ?? '',
       name: lineItem.name.en,
       price: lineItem.price.value.centAmount,
       quantity: lineItem.quantity,
       sku: lineItem.variant.sku ?? '',
-      // totalPrice:
     };
     basketItems.push(basketItem);
   });
+  const discountCodes: Array<string> = [];
+  data.body.discountCodes.forEach((item) => {
+    const code = item.discountCode.obj?.code;
+    if (code) {
+      discountCodes.push(code);
+    }
+  });
+
   const result: BasketData = {
     basketItems: basketItems,
+    discountCodes: discountCodes,
     totalBasketPrice: data.body.totalPrice.centAmount,
   };
+  const discountOnTotalPrice = data.body.discountOnTotalPrice?.discountedAmount.centAmount;
+  if (discountOnTotalPrice) {
+    result.discountOnTotalPrice = discountOnTotalPrice;
+  }
   return result;
 }
 
