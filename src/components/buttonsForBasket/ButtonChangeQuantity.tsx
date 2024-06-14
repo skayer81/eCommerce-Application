f2695c97836595d85829e4f0462cc5b1de76fed6
skayer81 <114329023+skayer81@users.noreply.github.com>
2024-06-14 10:26:46 +0300

    fix: ButtonChangeQuantity (#112)

--- src/components/buttonsForBasket/ButtonChangeQuantity.tsx
+++ src/components/buttonsForBasket/ButtonChangeQuantity.tsx
@@ -3,25 +3,22 @@ import { Button } from '@mui/material';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { changeNumberItemInBasket } from '@/api/clientService';
-import { findItemInBasket } from '@/components/findItemInBasket/findItemInBasket';
 import { useBasketStore } from '@/stores/basketStore';
-import { useUserStore } from '@/stores/userStore';
 
 function ButtonChangeQuantity({
+  ID,
   callback,
   children,
   disabled = false,
   quantity,
-  sku,
 }: {
+  ID: string;
   callback?: () => void;
   children: JSX.Element | string;
   disabled?: boolean;
   quantity: number;
-  sku: string;
 }): JSX.Element {
   const { updateCurrentVersion, basketId, basketVersion } = useBasketStore();
-  const { userId } = useUserStore();
   const queryClient = useQueryClient();
 
   const getBody = (listItemID: string): MyCartUpdate => {
@@ -37,19 +34,9 @@ function ButtonChangeQuantity({
     };
   };
 
-  const changeQuantity = async (sku: string, userId: string): Promise<ClientResponse<Cart>> => {
-    const liineItem = await findItemInBasket(sku, userId);
-    if (!liineItem) {
-      throw new Error('что то пошло не так');
-    }
-    const result = await changeNumberItemInBasket(getBody(liineItem.id), basketId);
-    return result;
-  };
-
   const { mutate } = useMutation<ClientResponse>({
-    mutationFn: () => changeQuantity(sku, userId),
+    mutationFn: () => changeNumberItemInBasket(getBody(ID), basketId),
     onSuccess: ({ body }: ClientResponse<Cart>) => {
-      console.log('lastBasket=', body);
       updateCurrentVersion(body.version);
       queryClient.invalidateQueries({ queryKey: ['basketList'] }).catch((error: Error) => {
         throw new Error(error.message);
