763c2c118c36b5665a6ea38e97bfbb43d56935c6
alekseng <alexvdche@mail.ru>
2024-06-15 20:33:53 +0300

    feat: add collaboration

--- src/features/aboutPage/AboutPage.tsx
+++ src/features/aboutPage/AboutPage.tsx
@@ -1,9 +1,11 @@
+import Collaboration from './Collaboration';
 import DeveloperCard from './DeveloperCard';
 import Rsschool from './Rsscool';
 
 function AboutPage(): JSX.Element {
   return (
     <>
+      <Collaboration />
       <DeveloperCard />
       <Rsschool />
     </>
